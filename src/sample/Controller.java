0cb17adbc60ffec91e8e42aa9e8ec6f5c82082f2
EmilDino <EmilLD123@hotmail.com>
2018-11-07 10:52:47 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Controller {
+
+    @FXML
+    public TextField textFelt;
+
+    public void textMetode(ActionEvent actionEvent) {
+        
+    }
+
+}
